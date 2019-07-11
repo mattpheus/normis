d743e04ad83f6cbcf241a996927515f84b705ae2
Mattpheus <mhseecha@gmail.com>
2019-07-11 12:18:43 -0400

    First commit adding in files

---
+++ script.js
@@ -0,0 +1,12 @@
+const start; //start button needs an input from user
+const counter = +1
+let userInput = []
+let nomis = Math.floor(Math.random())
+let failPattern = alert "Sorry you lost. Want to try again?"
+const yesButton;
+const noButton;
+
+
+
+
+
