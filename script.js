064461c281a3f0f808ee1555867f50c16fbdd6c8
Mattpheus <mhseecha@gmail.com>
2019-07-12 02:07:49 -0400

    p1 layout of functionality

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
-const start; //start button needs an input from user
-const counter = +1
-let userInput = []
-let nomis = Math.floor(Math.random())
-let failPattern = alert "Sorry you lost. Want to try again?"
-const yesButton;
-const noButton;
+
+let start = false; //Initilizing of the game
+
+let pattern = []; //order
+let nomisTurn; //computer turn
+let lightUp; //nomis light up
+
+let playerPattern = [];//player order
+let playerTurn; //player turn
+
+let goodMemory; //user is getting the memorization correct
+let turnCount; //interval turn number
+
+let win; //winning
+
+const startButton = document.querySelector('#start');
+const topLeft = document.querySelector('#top-left');
+const topRight = document.querySelector('#top-right');
+const bottomLeft = document.querySelector('#bottom-left');
+const bottomRight = document.querySelector('#bottom-right');
 
 
 
+startButton.addEventListener('click', (event) => {
+    if (start === true) {
+        gameTurn();
+    }
 
+});
+//MDN research
 
+function darkColor() {
+    topLeft.style.backgroundColor = 'background: #3ec914a2;';
+    topRight.style.backgroundColor = 'background: #b61515a2;'; 
+    bottomLeft.style.backgroundColor = 'background: #e8e52391;';
+    bottomRight.style.backgroundColor = 'background: #0c4e99a4;';
+
+}
+
+function brightColor() {
+    topLeft.style.backgroundColor = 'lightgreen';
+    topRight.style.backgroundColor = 'brightred';
+    bottomLeft.style.backgroundColor = 'brightyellow';
+    bottomRight.style.backgroundColor = 'brightblue';
+}
+
+topLeft.addEventListener('click',(event)) => {
+    if (start) {
+        playerPattern.push(1);
+    }
+}
+
+topRight.addEventListener('click',(event)) => {
+    if (start) {
+        playerPattern.push(1);
+    }
+}
+
+// function gameTurn(){
+//     start = false;
+//     if(start == pattern) {
+//         nomisTurn = false;
+//         start = true
+// }
+
+// function green (){
+
+//     topleft.style.backgroundColor = "lightgreen"
