1dadea242eba27e01a514d8498482dc693a7ada1
Mattpheus <mhseecha@gmail.com>
2019-07-15 16:21:53 -0400

    MVP

--- script.js
+++ script.js
@@ -13,7 +13,6 @@
 
 // let win; //winning
 
-let start = false;
 
 let userInput = []
 
@@ -24,6 +23,7 @@ const bottomRight = document.querySelector('#bottom-right');
 
 const startButton = document.querySelector('#start');
 
+let start = false;
 startButton.addEventListener('click', function(event) {
 	if (start === false) {
 		start === true;
@@ -37,6 +37,12 @@ let patternItems = [topLeft, topRight, bottomLeft, bottomRight]; //order
 let enumPatternItems = ["top-left", "top-right", "bottom-left", "bottom-right"];
 let currentPattern = []; //this is the new array from let enumPatternItems, so use this to log against what the user puts in.
 
+// Everytime user clicks, store clicked div in an array
+// compare user array with computer array
+// if user array = computer array: win
+// otherwise check length of user array === computer array? if so, they lose
+// otherwise continue
+
 function brightColor(index){
 	patternItems[index].style.opacity = 1;
 }
@@ -49,21 +55,34 @@ function darkColor() {
 
 const showColors = function() {
 	let order = Math.floor(Math.random() * patternItems.length);
+	darkColor();
 	brightColor(order);
 	currentPattern.push(enumPatternItems[order])
-	//console.log(enumPatternItems);
+	console.log(currentPattern); //don't forget to remove this when deploying
 	setTimeout(() => {
+		console.log('dark color is called') //don't forget to remove this when deploying
 		darkColor();
-	}, 850);
+	}, 800);
 		
 };
+
+const showUserColors = function() {
+	darkColor();
+	brightColor(index);
+	currentPattern.push(enumPatternItems[order])
+	console.log(showUserColors); //don't forget to remove this when deploying
+	setTimeout(() => {
+		console.log('dark color is called')
+		darkColor();
+	}, 800);
+}
 
 const stop = () => {
 	clearInterval(showColors);
 };
 
 
-let timer = 0;
+let timer = 1;
 const compStart = function() {
 	//computer turn playing. It generates the random number
 	const compTurn = setInterval(() => {
@@ -72,41 +91,102 @@ const compStart = function() {
 		if (timer === 5) {
 			clearInterval(compTurn);
 		}
-	}, 999);
-//console.log('computerTurnOn')
+	}, 900);
+console.log('computerTurnOn') //don't forget to remove this when deploying
 };
 
 function userClick(event){
+	console.log(event.target.id) //don't forget to remove this when deploying
 	return event.target.id
 }
 //This is the user input.
-let count = 0;
+// let count = 0;
 const userInputs = [];
-let userColorInput = function(event){
-	console.log(`Computer Pattern: ${currentPattern}`);
-	userInputs.push(userClick(event));	
-	//The bottom code represents the starting of my compare function.
-	if (currentPattern[count] != userInputs[count]) {
-		console.log("You Lose");
+const userColorInput = function(event){
+	userInputs.push(userClick(event))
+	if (currentPattern.length === userInputs.length) {
+		let compare = true;
+		for(let i = 0; i < currentPattern.length; i++) {
+			if (currentPattern[i] !== userInputs[i]) {
+				compare = false;
+				
+			}
+		}
+		compare ? alert('you win') : alert('you lose')
 	}
-	
-
-
-	
-	// console.log(`User Pattern: ${userInputs}`);
-	//let count = 0;
-	// return function(event) {
-	// 	count++;
-	// 	userInputs.push(userClick(event))
-	// 	console.log(userInputs)
-	// }
-	// for(let i = 0; i < 5; i++){
-	// 	let _userClick = userClick((event))
-	// 	userInputs.push(_userClick()) 
-	// }
-
-	count++;
+			// console.log ('compare win') //don't forget to remove this when deploying
+// count++;
 }
+	// else {
+	// 	console.log ("you lose")
+
+	// }
+
+// arrA = ["ham", "cheese"];
+// arrB = ["ham", "cheese"];
+
+// const check = function(arr1, arr2) {
+//   let equal = true;
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       equal = false;
+//     }
+//   }
+//   return equal;
+// };
+
+// console.log(check(arrA, arrB));
+// const winGame = function(){
+
+// }
+	
+
+
+
+	// 
+	// 	}
+	// for(let j = 0; j < userInputs.length; j++){
+	// 	if (userInputs[i] !== currentPattern.length){
+	// 		console.log ('You lose')
+	// 	}
+	// }
+
+	// function compare(arr1,arr2){
+  
+	// 	if(!arr1  || !arr2) return
+	   
+	// 	 let result;
+	   
+	//    arr1.forEach((e1,i)=>arr2.forEach(e2=>{
+		 
+	// 		  if(e1.length > 1 && e2.length){
+	// 			 result = compare(e1,e2);
+	// 		  }else if(e1 !== e2 ){
+	// 			 result = false
+	// 		  }else{
+	// 			 result = true
+	// 		  }
+	// 	 })
+	//    )
+	   
+	//    return result
+	   
+	//  }
+		// for(let j = 0; j < userInputs[j]; j++) {
+
+		// }
+		// call comparison here
+	
+	// else if (currentPattern.length !== userInputs.length ){
+	
+	
+	// console.log(userInputs)
+
+
+
+// function win(){
+
+// }
 
 
 // function countClicks(){
@@ -121,21 +201,16 @@ let userColorInput = function(event){
 
 
 // function compare(){
-// 	if (currentPattern[count] != userColorInput[count])
-// 	console.log ()
-// 	else { (currentPattern[count] == userColorInput[count])
-// 	}
+// 	 if {currentPattern.length ==! userInputs.length
+
+// 		console.log('Hold the L')}
+	
 // }
-// compare()
+
 
 
 // const checkPress = function(event) {
 // 	// if (event.target === currentPattern[0]) {
 // 	// 	console.log('yes');
 // 	// }
-// };
-
-
-// function winGame(){
-
 // }
